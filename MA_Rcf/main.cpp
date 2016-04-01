62c4ab9ba40d0e67f8de52f2e4e13d126f7dfe6e
唐向龙 <tangxianglong@macair.local>
2016-04-01 10:34:13 +0800

    Initial Commit

---
+++ MA_Rcf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MA_Rcf
+//
+//  Created by 唐向龙 on 16/4/1.
+//  Copyright © 2016年 唐向龙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
